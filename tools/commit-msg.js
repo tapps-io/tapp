96e8132f24db72c2dfc0b389a8d7a03219be1d24
Erik Hughes <erik.hughes@outlook.com>
2020-02-02 12:32:56 +0100

    build: improved changelog and commit tools

--- tools/commit-msg.js
+++ tools/commit-msg.js
@@ -5,18 +5,18 @@ if (/^(merge|chore\(release\))/i.test(commit)) {
   process.exit(0);
 }
 
-if (!/^(feat|fix|perf|revert|docs|style|refactor|test|build|ci)(?:\(.+\))?: /.test(commit)) {
+if (!/^(feat|fix|perf|revert|docs|style|refactor|test|build|ci|k8s)(?:\(.+\))?: /.test(commit)) {
   console.warn(
-    `Git commit message should lead with a type, followed by an optional scope in parentheses, then a colon, a space, and a message. "feat(typeahead): JRA-1337 added keyboard interaction".`,
+    'Git commit message should lead with a type, followed by an optional scope in parentheses, then a colon, a space, and a message. "feat(typeahead): #1337 added keyboard interaction".',
   );
 
-  console.warn(`Valid types are feat|fix|perf|revert|docs|style|refactor|test|build|ci.`);
+  console.warn('Valid types are feat|fix|perf|revert|docs|style|refactor|test|build|ci.');
 
-  if (/^(feat|fix)(?:\(.+?\))?: /.test(commit) && !/(#\d+|[A-Z0-9]{2,}-[0-9]+)/.test(commit)) {
-    console.warn(`Features and fixes changes should include a task id.`);
-    console.warn(`Example: "feat(typeahead): JRA-1337 added keyboard interaction".`);
+  if (/^(feat|fix)(?:\(.+?\))?: /.test(commit) && !/#\d+/.test(commit)) {
+    console.warn('Features and fixes changes should include a task id.');
+    console.warn('Example: "feat(typeahead): #1337 added keyboard interaction".');
   } else {
-    console.warn(`Example: "ci(changelog): build step added for conventional changelogs".`);
+    console.warn('Example: "ci(changelog): build step added for conventional changelogs".');
   }
 
   process.exit(1337);
