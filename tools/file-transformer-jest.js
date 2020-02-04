d3e1455a392b274c251d2efe29e068ccce3f3f83
Erik Hughes <erik.hughes@outlook.com>
2020-02-04 12:14:47 +0100

    test: #1 fixed file-transformer, relative to root

--- tools/file-transformer-jest.js
+++ tools/file-transformer-jest.js
@@ -6,7 +6,9 @@ module.exports = {
   process(src, filename, config, options) {
     // Fake url-loader
     return `module.exports = ${JSON.stringify(
-      `data:asset/${path.extname(filename).replace('.', '')};base64,${Buffer.from(filename).toString('base64')}`,
+      `data:asset/${path.extname(filename).replace('.', '')};base64,${Buffer.from(
+        path.relative(config.rootDir, filename).replace(/(\\{1,2})/, '/'),
+      ).toString('base64')}`,
     )}`;
   },
 };
